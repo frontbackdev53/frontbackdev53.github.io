f8c7f4f5fb517d61d82b1279cfceeabb478f7ef8
frontbackdev53 <frontbackdev53@gmail.com>
2021-11-02 12:36:22 +0200

    Updates

---
+++ precache-manifest.2e7022fc6e2cf57a2833330a1de04f84.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ede0da0f4f9f00f41f3c6c4d2ed75bb3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a7dedf0b4c015119be10",
+    "url": "/static/css/2.f7861fd5.chunk.css"
+  },
+  {
+    "revision": "a7dedf0b4c015119be10",
+    "url": "/static/js/2.b01ba843.chunk.js"
+  },
+  {
+    "revision": "d1a8c3b04f57735257e6",
+    "url": "/static/js/main.a62c65e0.chunk.js"
+  },
+  {
+    "revision": "39ea0ba043d04056de07",
+    "url": "/static/js/runtime-main.757209a5.js"
+  },
+  {
+    "revision": "db0ad2fc713ab72ea682687be4bd1021",
+    "url": "/static/media/Quicksand-Medium.db0ad2fc.ttf"
+  },
+  {
+    "revision": "935b8c175544c36d3fd460baa040eab3",
+    "url": "/static/media/Vector.935b8c17.svg"
+  },
+  {
+    "revision": "02ca80b40cc8948fd88ca7f345612d24",
+    "url": "/static/media/howit.02ca80b4.svg"
+  }
+]);
